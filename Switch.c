533b68a079801dc74272a2d6cc1501b403a00cda
YashBhangale216 <bhangaleyash912@gmail.com>
2024-06-09 17:45:53 +0530

    Fixed Switch Prog

--- Switch.c
+++ Switch.c
@@ -3,6 +3,36 @@
 
 int main()
 {
-    printf("This is a new repo.");
+    char operator; 
+    double num1, num2;
+    printf("Enter An Operator (+,-,*,/)");
+    scanf("%c",&operator);
+    printf("Enter The Value:");
+    scanf("%lf",&num1);
+    printf("Enter Second Value:");
+    scanf("%lf",&num2);
+
+    switch (operator)
+    {
+        case '+':
+        printf("%1lf",num1+num2);
+        break;
+
+        case '-':
+        printf("%1lf",num1-num2);
+        break;
+
+        case '*':
+        printf("%1lf",num1*num2);
+        break;
+
+        case '/':
+        printf("%1lf",num1/num2);
+        break;
+
+        default:
+        printf("Error !! Operator Is Not Found");
+    }
+
     return 0;
 }
